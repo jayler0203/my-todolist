f77c9f947e7310a604bee9a3b37ae2efa3aee0dd
jayler0203 <jayler0203@gmail.com>
2023-11-15 19:04:21 -0500

    refactor:mover funcion de toggleTask a App.js

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,26 @@ import FormTodo from './components/FormTodo';
 import TaskList from './components/TaskList';
 function App() {
 const [tasks, setTasks] = useState([]);
+const [checkedTasks, setCheckedTasks] = useState([]);
 const addTask = (task) => {
 setTasks([...tasks, task]);
+setCheckedTasks([...checkedTasks, false])
 };
 const deleteTask = (index) => {
 const updatedTasks = tasks.filter((_, i) => i !== index);
 setTasks(updatedTasks);
 };
+const toggleTask =(index)=>{
+    const updatedCheckedTasks = [...checkedTasks];
+    updatedCheckedTasks[index] = !updatedCheckedTasks[index];
+    setCheckedTasks(updatedCheckedTasks);
+}
 return (
 <div>
 <Container>
 <h1>To-Do List</h1>
 <FormTodo onAddTask={addTask} />
-<TaskList tasks={tasks} onDeleteTask={deleteTask} />
+<TaskList tasks={tasks} onDeleteTask={deleteTask} onToggleTask={toggleTask} checkedTask={checkedTasks}/>
 </Container>
 </div>
 );
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import Checkbox from "./Checkbox"
 import { useState } from 'react';
-const TaskList = ({ tasks, onDeleteTask }) => {
-  const [checkedItems, setCheckedItems] = useState([]);
-  const handlecheck =(index)=>{
-      const updatedCheckedItems = [...checkedItems];
-      updatedCheckedItems[index] = !updatedCheckedItems[index];
-      setCheckedItems(updatedCheckedItems);
-
-  }
+const TaskList = ({ tasks, onDeleteTask, checkedTask,onToggleTask }) => {
 return (
 <ul>
 {tasks.map((task, index) => (
 <li key={index} >
-  {console.log(checkedItems[index])}
-  <Checkbox isChecked={checkedItems[index]?checkedItems[index]:false} onChange={()=>handlecheck(index)} ></Checkbox>
-<p style={{textDecoration:checkedItems[index]?"line-through":"none"}}>{task}</p>
+  <Checkbox isChecked={checkedTask[index]} onChange={()=>onToggleTask(index)} ></Checkbox>
+<p style={{textDecoration:checkedTask[index]?"line-through":"none"}}>{task}</p>
 <button onClick={() => onDeleteTask(index)}>Delete</button>
 </li>
 ))}
